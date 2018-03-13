0a1773f46faaeaaedc5a2c0f698ac377b798b572
Gabriel Sochacki <gabriel-sochacki@o2.pl>
2018-03-13 20:19:30 +0100

    first commit

---
+++ src/mediaplayer/MediaPlayer.java
@@ -0,0 +1,46 @@
+
+package mediaplayer;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lumi
+ */
+public class MediaPlayer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("WTFplayer");
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+                if(doubleClicked.getClickCount()==2)
+                {
+                    stage.setFullScreen(true);                   
+                }
+            }
+        });
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
